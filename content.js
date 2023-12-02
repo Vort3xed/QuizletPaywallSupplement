59bb635e212d83dd1b0766eeade4adc4777a2865
Agneya T <agneyat2@gmail.com>
2023-12-02 15:20:14 -0500

    remove blur for using quizlet with no account

--- content.js
+++ content.js
@@ -1,105 +1,93 @@
 // content.js
-// const divClassToRemove = "ReactModal__Overlay ReactModal__Overlay--after-open oz4nps6 openAnimation";
 const divClassToRemove = "ReactModalPortal";
 
-// function clickButtonByClass(className) {
-//     const button = document.querySelector(`.${className}`);
-//     if (button) {
-//       button.click();
-//     }
-// }
+function removeStringFromClassDefinitions(targetString) {
 
-// function removeDiv() {
-//   const divToRemove = document.querySelector(`div.${divClassToRemove}`);
-//   if (divToRemove) {
-//     divToRemove.remove();
-//     clickButtonByClass("AssemblyButtonBase AssemblyPrimaryButton--default AssemblyButtonBase--large AssemblyButtonBase--padding AssemblyButtonBase--fullWidth");
-//   }
-// }
+  let hasDivsWithString = true;
 
-// // Listen for messages from the popup
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   if (message.action === "removeDiv") {
-//     removeDiv();
-//   }
-// });
+  while (hasDivsWithString) {
+    const divElements = document.querySelectorAll("div");
 
+    hasDivsWithString = false;
+
+    for (const div of divElements) {
+      if (div.classList.contains(targetString)) {
+        div.classList.remove(targetString);
+        hasDivsWithString = true;
+      }
+    }
+  }
+}
 
 
 function removeDivByClass(className) {
-    const divToRemove = document.querySelector(`.${className}`);
-    if (divToRemove) {
-      divToRemove.remove();
-    }
+  const divToRemove = document.querySelector(`.${className}`);
+  if (divToRemove) {
+    divToRemove.remove();
   }
-  
-  // Function to click the button with the specified class
-  function clickButtonByClass(className) {
-    const button = document.querySelector(`.${className}`);
-    if (button) {
-      button.click();
-    }
-  }
+}
 
-  function clickButtonByAriaLabel(ariaLabel) {
-    const button = document.querySelector(`[aria-label="${ariaLabel}"]`);
-    if (button) {
-      button.click();
+function clickButtonByClass(className) {
+  const button = document.querySelector(`.${className}`);
+  if (button) {
+    button.click();
+  }
+}
+
+function clickButtonByAriaLabel(ariaLabel) {
+  const button = document.querySelector(`[aria-label="${ariaLabel}"]`);
+  if (button) {
+    button.click();
+  }
+}
+function startScroll() {
+  let scrollDistance = 0;
+  const scrollInterval = setInterval(() => {
+    window.scrollBy(0, 10);
+    scrollDistance += 10;
+
+    if (scrollDistance >= 200) {
+      clearInterval(scrollInterval);
     }
-  }
-  function startScroll() {
-    // Set an interval to scroll down gradually
-    let scrollDistance = 0;
-    const scrollInterval = setInterval(() => {
-      window.scrollBy(0, 10); // Scroll down by 10 pixels
-      scrollDistance += 10;
-      
-      // Adjust the scrolling duration as needed
-      if (scrollDistance >= 200) {
-        clearInterval(scrollInterval); // Stop scrolling after reaching a certain distance
-      }
-    }, 100); // Scroll every 100 milliseconds
-  }
+  }, 100);
+}
 
 
-  function startScrollDown() {
-    // Set an interval to scroll down gradually
-    let scrollDistance = 0;
-    const scrollInterval = setInterval(() => {
-      window.scrollBy(0, 10); // Scroll down by 10 pixels
-      scrollDistance += 10;
-      
-      // Adjust the scrolling duration as needed
-      if (scrollDistance >= 200) {
-        clearInterval(scrollInterval); // Stop scrolling after reaching a certain distance
-      }
-    }, 100); // Scroll every 100 milliseconds
+function startScrollDown() {
+  console.log("scrolldown has been called");
+  let scrollDistance = 0;
+  const scrollInterval = setInterval(() => {
+    window.scrollBy(0, 10);
+    scrollDistance += 10;
+    if (scrollDistance >= 200) {
+      clearInterval(scrollInterval);
+    }
+  }, 100);
+}
+
+
+function startScrollUp() {
+  console.log("scrollup has been called");
+  let scrollDistance = 0;
+  const scrollInterval = setInterval(() => {
+    window.scrollBy(0, -10);
+    scrollDistance += 10;
+
+    if (scrollDistance >= 200) {
+      clearInterval(scrollInterval);
+    }
+  }, 100);
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "removeDiv") {
+    removeDivByClass("ReactModalPortal");
+    clickButtonByAriaLabel("Show all steps");
+  } else if (message.action === "startScrollDown") {
+    startScrollDown();
+  } else if (message.action === "startScrollUp") {
+    startScrollUp();
+  } else if (message.action === "removeStringFromClass") {
+    removeStringFromClassDefinitions("b1sa2ccx");
   }
-  
-  // Function to start scrolling up
-  function startScrollUp() {
-    // Set an interval to scroll up gradually
-    let scrollDistance = 0;
-    const scrollInterval = setInterval(() => {
-      window.scrollBy(0, -10); // Scroll up by 10 pixels
-      scrollDistance += 10;
-      
-      // Adjust the scrolling duration as needed
-      if (scrollDistance >= 200) {
-        clearInterval(scrollInterval); // Stop scrolling after reaching a certain distance
-      }
-    }, 100); // Scroll every 100 milliseconds
-  }
-  
-  // Listen for messages from the popup
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "removeDiv") {
-      removeDivByClass("ReactModalPortal");
-      clickButtonByAriaLabel("Show all steps");
-    //   startScroll();
-    }else if (message.action === "startScrollDown") {
-        startScrollDown();
-      } else if (message.action === "startScrollUp") {
-        startScrollUp();
-      }
-  });
+});
